89a6c536ac29ab1c9e8ea371cecc49c9151c50e2
kunalash <kunalash.it17@gmail.com>
2025-07-21 12:07:05 +0530

    Moved the CAR and CCIR values to properties file

--- certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/CARReceiptDataProvider.java
+++ certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/CARReceiptDataProvider.java
@@ -3,40 +3,44 @@ package io.mosip.certify.api.dataprovider.impl;
 import io.mosip.certify.api.dataprovider.DataProviderService;
 import io.mosip.certify.api.service.MapImageGeneratorService;
 import org.json.JSONObject;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Map;
 
 @Component
 public class CARReceiptDataProvider implements DataProviderService {
     private final MapImageGeneratorService mapImageGeneratorService;
 
-    @Override
-    public String getDocumentType() {
-        //Changed the data provider type to CARReceipt to match with certify json key to integrate with govbr
-        return "CARReceipt";
-    }
-
     private final WebClient webClient;
-
-    // OAuth2 token endpoint
-    private final String tokenUrl = "https://pisrj.dataprev.gov.br/oauth2/token";
-
-    // Client credentials (replace with your actual values)
-    private final String clientId = "xdDsOG5vguj7QfxzMeRU1idlDd0a";
-    private final String clientSecret = "ezvQJcrbWjO4gSZtHRHYoMQP7W8a";
 
     // Optional scope (empty string if not needed)
     private final String scope = "";
 
-    // Protected API endpoint to call with token
-    private final String apiUrl = "https://papisp.dataprev.gov.br/sicar/reciboDegustacao/1.0/BA-2902005-AD06AC9BEE924477AA13EFE0908D15E0";
+    @Value("${car.receipt.api.url}")
+    private final String apiUrl;
 
-    public CARReceiptDataProvider(MapImageGeneratorService mapImageGeneratorService, WebClient.Builder webClientBuilder) {
+    private final CarTokenClient carTokenClient;
+
+    public CARReceiptDataProvider(MapImageGeneratorService mapImageGeneratorService,
+                                  WebClient.Builder webClientBuilder,
+                                  @Value("${car.receipt.api.url}") String apiUrl,
+                                  CarTokenClient carTokenClient) {
         this.mapImageGeneratorService = mapImageGeneratorService;
         this.webClient = webClientBuilder.build();
+        this.apiUrl = apiUrl;
+        this.carTokenClient = carTokenClient;
+    }
+
+    /**
+     * Returns the document type for this data provider.
+     * This is used to identify the type of data being provided.
+     *
+     * @return The document type as a String.
+     */
+    @Override
+    public String getDocumentType() {
+        //Changed the data provider type to CARReceipt to match with certify json key to integrate with govbr
+        return "CARReceipt";
     }
 
     /**
@@ -45,10 +49,10 @@ public class CARReceiptDataProvider implements DataProviderService {
      */
     public JSONObject getData() throws Exception {
         // Step 1: Get access token
-        String accessToken = getAccessToken();
+        String accessToken = carTokenClient.getAccessToken();
 
         // Step 2: Call protected API with Bearer token
-        String response =  webClient.get()
+        String response = webClient.get()
                 .uri(apiUrl)
                 .headers(headers -> headers.setBearerAuth(accessToken))
                 .retrieve()
@@ -59,46 +63,6 @@ public class CARReceiptDataProvider implements DataProviderService {
         }
         JSONObject jsonObject = new JSONObject(response);
         return new JSONObject((jsonObject.getJSONArray("result").get(0)).toString())
-                .put("geoImovel", mapImageGeneratorService.generateMapImage(((JSONObject)jsonObject.getJSONArray("result").get(0)).get("geoImovel").toString()));
-    }
-    /**
-     * Retrieves the OAuth 2.0 token from the token endpoint using client credentials grant.
-     */
-    private String getAccessToken() {
-        Map tokenResponse = webClient.post()
-                .uri(tokenUrl)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .bodyValue("grant_type=client_credentials" +
-                        "&client_id=" + clientId +
-                        "&client_secret=" + clientSecret +
-                        "&scope=default" )
-                .retrieve()
-                .bodyToMono(Map.class)
-                .block();
-
-        if (tokenResponse == null || tokenResponse.get("access_token") == null) {
-            throw new RuntimeException("Failed to retrieve access token");
-        }
-
-        return (String) tokenResponse.get("access_token");
-    }
-
-    /**
-     * Class representing the OAuth token JSON response.
-     * Maps JSON keys to Java fields.
-     */
-    public static class TokenResponse {
-        private String access_token;
-        private String token_type;
-        private long expires_in;
-        private String scope;
-
-        public String getAccessToken() {
-            return access_token;
-        }
-
-        public void setAccessToken(String access_token) {
-            this.access_token = access_token;
-        }
+                .put("geoImovel", mapImageGeneratorService.generateMapImage(((JSONObject) jsonObject.getJSONArray("result").get(0)).get("geoImovel").toString()));
     }
 }
