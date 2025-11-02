cabcf00766a28c2c1e49115ce503bd4c3d774362
kunalash1 <kunal.ash@in.ey.com>
2025-11-02 23:13:24 +0530

    Added wire mock implementation for caf

--- certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/CAFDataProvider.java
+++ certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/CAFDataProvider.java
@@ -1,39 +1,61 @@
 package io.mosip.certify.api.dataprovider.impl;
 
 import io.mosip.certify.api.dataprovider.DataProviderService;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@Slf4j
 @Component
 public class CAFDataProvider implements DataProviderService {
+    private final SicarCpfCnpjClient sicarCpfCnpjClient;
+
+    private final WebClient webClient;
+
+    private final String scope = "";
+
+    private final String apiUrl;
+
+    private final CafTokenClient carTokenClient;
+
+    public CAFDataProvider(SicarCpfCnpjClient sicarCpfCnpjClient, WebClient webClient, @Value("${caf.api.url}")String apiUrl, CafTokenClient carTokenClient) {
+        this.sicarCpfCnpjClient = sicarCpfCnpjClient;
+        this.webClient = webClient;
+        this.apiUrl = apiUrl;
+        this.carTokenClient = carTokenClient;
+    }
 
     @Override
     public String getDocumentType() {
         return "CAFCredential";
     }
 
+    /**
+     * Main method to be called after your validations.
+     * It fetches the OAuth2 token, then calls the protected API using the token.
+     *
+     * @return JSONObject containing the data from the CAR API.
+     * @throws JSONException if there is an error with JSON operations.
+     */
     @Override
     public JSONObject getData(String cpfNumber) throws JSONException {
-        return new JSONObject()
-                .put("enquadramentoPronaf", "Simples Nacional")
-                .put("situacao", "Ativo")
-                .put("dataCriacao", "2023-01-01")
-                .put("dataValidade", "2026-01-01")
-                .put("numeroCaf", "CAF123456789")
-                .put("Ultima_Atualizacao", "2025-07-15")
-                .put("atividadeprincipalUFPA", "Agricultura")
-                .put("caracterizacaoArea", "Área Rural")
-                .put("membros_tipoMembro_descricao", "Titular")
-                .put("possuiMaoObraContratada", "true")
-                .put("membros_nome", "João Silva")
-                .put("membros_cpf", "123.456.789-00")
-                .put("areas_condicaoPosse", "Proprietário")
-                .put("areas_tamanho", 10.5)
-                .put("areas_tamanho_areas_unidadeMedida_descricao", "10.5 hectares")
-                .put("municipio", "São Paulo")
-                .put("entidadeEmissora_razaoSocial", "Instituto Nacional de Agricultura")
-                .put("entidadeEmissora_cnpj", "12.345.678/0001-99")
-                .put("emissor", "Sistema CAF");
+        // Step 1: Get access token
+        String accessToken = carTokenClient.getAccessToken();
+        String registrationNumber = sicarCpfCnpjClient.getRegistrationNumber(cpfNumber, accessToken);
+        log.info("Registration Number: {}", registrationNumber);
+        // Step 2: Call protected API with Bearer token
+        String response =  webClient.get()
+                .uri(String.format(apiUrl, cpfNumber))
+                .headers(headers -> headers.setBearerAuth(accessToken))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        if (response == null) {
+            throw new RuntimeException("Failed to retrieve data from the API");
+        }
+        return new JSONObject(response);
     }
 }
