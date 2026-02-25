0e70adbc1c0722e310d0b59efd2a018dd5819422
Alexandre Dekker <alexandre.dekker@dataprev.gov.br>
2026-02-25 15:53:37 -0300

    versao 4.1.2 p teste

--- certify-integration-api/src/main/java/io/mosip/certify/api/config/WebClientConfig.java
+++ certify-integration-api/src/main/java/io/mosip/certify/api/config/WebClientConfig.java
@@ -1,14 +1,43 @@
 package io.mosip.certify.api.config;
-
+ 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-
+import reactor.netty.http.client.HttpClient;
+ 
+import javax.net.ssl.SSLException;
+import java.util.Arrays;
+ 
+@Slf4j
 @Configuration
 public class WebClientConfig {
-
+ 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    public WebClient webClient() throws SSLException {
+        log.info("Configuring WebClient with custom cipher suites");
+ 
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .protocols("TLSv1.2", "TLSv1.3")
+                .ciphers(Arrays.asList(
+                        "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+                        "TLS_RSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_RSA_WITH_AES_128_GCM_SHA256"
+                ))
+                .build();
+ 
+        HttpClient httpClient = HttpClient.create()
+                .secure(sslSpec -> sslSpec.sslContext(sslContext));
+ 
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
+ 
+ 
