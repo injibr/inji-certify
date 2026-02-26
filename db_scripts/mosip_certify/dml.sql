bb038f97edc59acb811b6bf229f780db2f557947
Fred Guth <fredguth@fredguth.com>
2026-02-26 06:55:26 -0300

    fix: conserta erro 424 do script com Dockerfile

--- db_scripts/mosip_certify/dml.sql
+++ db_scripts/mosip_certify/dml.sql
@@ -5,20 +5,7 @@
 
 \c inji_certify
 
-
 \COPY certify.key_policy_def (APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) FROM './dml/certify-key_policy_def.csv' delimiter ',' HEADER  csv;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+\ir dml/certify-credential_template.sql
+\ir dml/certify-certify_keys.sql
