bf86867b3b3c00946c16968110416c50321aa14e
Alexandre Dekker <alexandre.dekker@dataprev.gov.br>
2026-03-06 11:02:07 -0300

    feat: remocao das demais faixas etarias

--- certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/EcaDataProvider.java
+++ certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/EcaDataProvider.java
@@ -56,9 +56,6 @@ public class EcaDataProvider implements DataProviderService {
         log.info("CPF {} - dataNascimento: {}, age: {}", cpfNumber, dataNascimento, age);
 
         JSONObject result = new JSONObject();
-        result.put("isOver12", age >= 12);
-        result.put("isOver14", age >= 14);
-        result.put("isOver16", age >= 16);
         result.put("isOver18", age >= 18);
         return result;
     }
