584ced6ab15cdcb2f9822f427e49dd9f69dfd50c
Paul Gerhard Schulze <paul.schulze@dataprev.gov.br>
2026-03-17 13:02:35 -0300

    SNCR  has a two-step approach for getting its data, as CAR

---
+++ certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/SncrCpfCnpjClient.java
@@ -0,0 +1,85 @@
+package io.mosip.certify.api.dataprovider.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Client for retrieving registration numbers based on CPF numbers from the Sicar service.
+ * This client uses WebClient to make HTTP requests to the specified URL.
+ */
+@Slf4j
+@Component
+public class SncrCpfCnpjClient {
+    private final WebClient webClient;
+    private final String url;
+
+    public SncrCpfCnpjClient(WebClient webClient,
+                              @Value("${ccir.registration.number.url}") String url) {
+        this.webClient = webClient;
+        this.url = url;
+    }
+
+    /**
+     * Retrieves the registration number for a given CPF number.
+     *
+     * @param cpfNo the CPF number to look up
+     * @return the registration number associated with the CPF
+     */
+    public String getRegistrationNumber(String cpfNo, String accessToken) {
+        log.info("Fetching registration number for CPF: {}", cpfNo);
+        String response = webClient.get()
+                .uri(String.format(url, cpfNo))
+                .headers(headers -> headers.setBearerAuth(accessToken))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode;
+        try {
+            rootNode = mapper.readTree(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        JsonNode firstCodigoImovel = rootNode
+                .path("result")
+                .path(0)
+                .path("codigoImovel");
+
+        return firstCodigoImovel.asText();
+    }
+
+       /**
+     * Retrieves the registration number for a given CPF number.
+     *
+     * @param cpfNo the CPF number to look up
+     * @return the registration number associated with the CPF
+     */
+    public String getNextRegistrationNumber(String cpfNo, String accessToken, int index) {
+        log.info("Fetching next registration number for CPF: {}", cpfNo);
+        String response = webClient.get()
+                .uri(String.format(url, cpfNo))
+                .headers(headers -> headers.setBearerAuth(accessToken))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode;
+        try {
+            rootNode = mapper.readTree(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        JsonNode indexCodigoImovel = rootNode
+                .path("result")
+                .path(index-1)
+                .path("codigoImovel");
+
+        return indexCodigoImovel.asText();
+    }
+
+}
