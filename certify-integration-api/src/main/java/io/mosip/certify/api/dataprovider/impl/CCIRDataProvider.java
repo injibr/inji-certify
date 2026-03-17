5f0fde66ae6ae634d879bf97667dca78910a9bb3
Paul Gerhard Schulze <paul.schulze@dataprev.gov.br>
2026-03-17 16:04:39 -0300

    Correção CCIR

--- certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/CCIRDataProvider.java
+++ certify-integration-api/src/main/java/io/mosip/certify/api/dataprovider/impl/CCIRDataProvider.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
